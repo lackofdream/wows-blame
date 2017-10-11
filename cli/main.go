9019571d2ddb62075af2dbecd978ecb804fbf1a2
LackOfDream <galaxy0419@gmail.com>
2017-10-11 23:06:23 +0900

    automaticly open browser

--- cli/main.go
+++ cli/main.go
@@ -4,9 +4,15 @@ import (
 	"net/http"
 
 	. "github.com/lackofdream/wows-blame"
+	"time"
+	"github.com/skratchdot/open-golang/open"
 )
 
 func main() {
+	go func() {
+		time.Sleep(1 * time.Second)
+		open.Start("http://localhost:8080")
+	}()
 	http.Handle("/", Router)
 	http.ListenAndServe(":8080", nil)
 }
